2cd2325acfdf510c7355110ebbc7ca73bb7fd009
Alexander Surma <surma@drebesium.org>
2010-02-01 00:24:10 +0100

    Commented header, added mode, temporary options

--- tagfs.c
+++ tagfs.c
@@ -1,7 +1,10 @@
 #define FUSE_USE_VERSION 26
-#include <stdio.h>
-#include <fuse.h>
-#include <db.h>
+#include <stdlib.h> // getuid() getgid()
+#include <sys/types.h>
+
+#include <fuse.h> // FUSE... duh
+#include <db.h> // BerkeleyDB
+#include <errno.h> // Return values
 
 #define FUSE_OP(a) .a = &tagfs_##a 
 
@@ -13,8 +16,19 @@ static void *tagfs_init(struct fuse_conn_info *conn) {
 }
 
 static int tagfs_getattr(const char *path, struct stat *stbuf) {
-	stat(path, stbuf) ;
-	return 0;
+	stbuf->st_uid = getuid() ;
+	stbuf->st_gid = getgid() ;
+	stbuf->st_blksize = 512 ;
+
+	if(strcmp(path,"/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0777 ;
+		stbuf->st_blocks = 0 ;
+		stbuf->st_nlink = 1 ;
+		stbuf->st_ino = 1 ;
+		stbuf->st_size = 0 ;
+		return 0;
+	}
+	return -ENOENT ;
 }
 
 static int tagfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
@@ -35,9 +49,14 @@ int main(int argc, char **argv) {
 	int i ;
 	struct fuse_args args = FUSE_ARGS_INIT(0, NULL) ;
 
-	for(i = 0; i < argc - 2; i++) 
+	fuse_opt_add_arg(&args, argv[0]) ;
+	fuse_opt_add_arg(&args, "-o") ;
+	fuse_opt_add_arg(&args, "use_ino") ;
+	for(i = 1; i < argc-2 ; i++) 
 		fuse_opt_add_arg(&args, argv[i]) ;
-	fuse_opt_add_arg(&args, argv[argc-1]) ;
+	if(argc > 2)
+		fuse_opt_add_arg(&args, argv[argc-1]) ;
+
 
 	return fuse_main(args.argc, args.argv, &tagfs_ops, argv[argc-2]) ;
 }
